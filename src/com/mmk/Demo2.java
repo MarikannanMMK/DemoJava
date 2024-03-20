14b9654a0ca4284a46c1877fc6ebc1f2c1e26f20
mariinbumurugan <109297860+mariinbumurugan@users.noreply.github.com>
2024-03-20 19:14:48 +0530

    Code updated with Demo2 again

--- src/com/mmk/Demo2.java
+++ src/com/mmk/Demo2.java
@@ -4,6 +4,8 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("MMK2");
         System.out.println("MMK2");
+        System.out.println("MMK2");
+
 
     }
 }
